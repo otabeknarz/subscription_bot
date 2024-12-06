a0e9d407fc59cbad32fdc9840f1e750477421670
Otabek Narziqulov <otabeknarz@gmail.com>
2024-12-06 20:18:02 +0500

    some changes in rate_limiter

--- modules/rate_limiter.py
+++ modules/rate_limiter.py
@@ -1,42 +1,75 @@
 import asyncio
 from functools import lru_cache
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-class RateLimiting:
-    LIMIT: int = 25
-    INTERVAL: int = 60  # in seconds
-
-    def __init__(self):
+class RateLimiter:
+    def __init__(self, bot_id: str, bot_username: str):
+        self.LIMIT: int = 25
+        self.INTERVAL: int = 1000  # in milliseconds
         self.QUEUE: list = []
         self.SENT_MESSAGES: int = 0
+        self.bot_id: str = bot_id
+        self.bot_username: str = bot_username
+        self.ALL_SENT_MESSAGES: int = 0
+
+    def __str__(self):
+        return f"Rate limiter for bot @{self.bot_username}"
+
+    def __repr__(self):
+        return f"RateLimiter(bot_id={self.bot_id}, bot_username={self.bot_username}, sent_messages={self.SENT_MESSAGES}, queue_length={len(self.QUEUE)})"
+
+    def __len__(self):
+        """Return the length of the messages queue"""
+        return len(self.QUEUE)
 
     # Methods for messages
-    async def respond(self, event, message):
-        if await self.check_for_queue():
-            self.SENT_MESSAGES += 1
-            await event.respond(message)
+    async def respond(self, event, *args, **kwargs):
+        rate_limiter = get_rate_limiter_from_memory(self.bot_id, self.bot_username)
+        if await rate_limiter.check_for_queue():
+            rate_limiter.SENT_MESSAGES += 1
+            rate_limiter.ALL_SENT_MESSAGES += 1
+            await event.respond(*args, **kwargs)
         else:
-            await self.add_to_queue(event.respond(message))
+            await rate_limiter.add_to_queue(event.respond(*args, **kwargs))
+
+    async def delete(self, event, *args, **kwargs):
+        rate_limiter = get_rate_limiter_from_memory(self.bot_id, self.bot_username)
+        if await rate_limiter.check_for_queue():
+            rate_limiter.SENT_MESSAGES += 1
+            rate_limiter.ALL_SENT_MESSAGES += 1
+            await event.delete(*args, **kwargs)
+        else:
+            await rate_limiter.add_to_queue(event.delete(*args, **kwargs))
 
     # Methods for controlling queues
     async def add_to_queue(self, task):
-        self.QUEUE.append(task)
+        rate_limiter = get_rate_limiter_from_memory(self.bot_id, self.bot_username)
+        rate_limiter.QUEUE.append(task)
 
     async def check_for_queue(self):
-        if self.SENT_MESSAGES >= self.LIMIT:
+        rate_limiter = get_rate_limiter_from_memory(self.bot_id, self.bot_username)
+        if rate_limiter.SENT_MESSAGES >= rate_limiter.LIMIT:
             return False
         else:
             return True
 
     async def periodic_check(self):
+        rate_limiter = get_rate_limiter_from_memory(self.bot_id, self.bot_username)
         while True:
-            await asyncio.sleep(self.INTERVAL / 60)
-            self.SENT_MESSAGES = 0
-            messages_task = self.QUEUE[:self.LIMIT]
-            self.QUEUE = self.QUEUE[self.LIMIT:]
-            await asyncio.gather(*messages_task)
+            await asyncio.sleep(rate_limiter.INTERVAL / 1000)
+            rate_limiter.SENT_MESSAGES = 0
+            messages_task = rate_limiter.QUEUE[:rate_limiter.LIMIT]
+            rate_limiter.QUEUE = rate_limiter.QUEUE[rate_limiter.LIMIT:]
+            for message_task in messages_task:
+                await message_task
 
 
 @lru_cache
-def get_rate_limiting() -> RateLimiting:
-    return RateLimiting()
+def get_rate_limiter_from_memory(bot_id: str, bot_username: str) -> RateLimiter:
+    """
+    Get rate limiter from memory, every bot has own rate limiter instance in memory
+    """
+    return RateLimiter(bot_id, bot_username)
